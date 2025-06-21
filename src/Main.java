a23d1ef54d66a4c2604a4a057d05222fb811d5fb
Wehttam10 <yihonglin94@gmail.com>
2025-06-21 16:38:09 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,33 @@
-//libary member, loan record, physicsal, digital,
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("--------------------------");
+        System.out.println("Library Management System");
+        System.out.println("--------------------------");
+        System.out.println("1. New Library Member Registration");
+        System.out.println("2. Borrow / Access Resources");
+        System.out.println("3. Manage Member Account (Credits/Fines)");
+        System.out.println("4. View / Return Loans");
+        System.out.println("5. Exit");
+        System.out.println("Select option (1 - 5) >>");
+        int option = input.nextInt();
+
+        if (option >= 1 && option <= 5) {
+            switch (option) {
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+                case 5:
+
+            }
+        }
+    }
+}
+
